e903a06ae8f34501cec4f7b566efff53f405595d
KWJ <81617589+pq5910@users.noreply.github.com>
2024-08-06 18:41:51 +0900

    fix: 연도 소비 차트 css 수정

--- src/components/Main/ChartMonth.vue
+++ src/components/Main/ChartMonth.vue
@@ -1,18 +1,20 @@
 <template>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    
   <div class="container">
     <h1 class="title"> 월별 소비 기록 </h1>
+    
+    <!-- 연도 선택 -->
+    <div class="year-selection">
+      <button @click="decrementYear"><span class="material-symbols-rounded icon">arrow_back_ios</span></button>
+      <span>{{ selectedYear }}</span>
+      <button @click="incrementYear"><span class="material-symbols-rounded icon">arrow_forward_ios</span></button>
+    </div>
     
     <div class="chart-container">
       <canvas id="expenseChart"></canvas>
     </div>
-  </div>
-      <div class="year-select-container">
-        <label for="year-select">년도 선택: </label>
-        <select id="year-select" v-model="selectedYear" @change="fetchData">
-          <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
-        </select>
-      </div>
-      <div class="list-container">
+    <div class="list-container">
         <div class="list-column">
           <div v-for="(expense, index) in firstHalfExpenses" :key="index" class="expense-item">
             <span class="month">{{ expense.month }}월</span>
@@ -32,184 +34,243 @@
           </div>
         </div>
       </div>
-  </template>
+  </div>      
+</template>
   
-  <script>
-  import axios from 'axios';
-  import Chart from 'chart.js/auto';
+<script>
+import axios from 'axios';
+import Chart from 'chart.js/auto';
   
-  export default {
-    data() {
-      return {
-        selectedYear: null,
-        years: [],
-        expenses: [],
-        chartInstance: null // Chart 인스턴스를 저장하기 위한 변수
-      };
+export default {
+  data() {
+    return {
+      selectedYear: new Date().getFullYear(),
+      years: [],
+      availableYears: [], // 사용 가능한 연도를 저장하기 위한 배열
+      expenses: [],
+      chartInstance: null // Chart 인스턴스를 저장하기 위한 변수
+    };
+  },
+  computed: {
+    firstHalfExpenses() {
+      const halfLength = Math.ceil(this.expenses.length / 2);
+      return this.expenses.slice(0, halfLength);
     },
-    computed: {
-      firstHalfExpenses() {
-        const halfLength = Math.ceil(this.expenses.length / 2);
-        return this.expenses.slice(0, halfLength);
-      },
-      secondHalfExpenses() {
-        const halfLength = Math.ceil(this.expenses.length / 2);
-        return this.expenses.slice(halfLength);
+    secondHalfExpenses() {
+      const halfLength = Math.ceil(this.expenses.length / 2);
+      return this.expenses.slice(halfLength);
+    }
+  },
+  methods: {
+    async fetchYears() {
+      const params = new URLSearchParams();
+      params.append('member_Id', '1'); // member_Id로 설정
+  
+      try {
+        const response = await axios.get(`/api/record/year?${params.toString()}`);
+        // console.log('Year response data:', response.data);
+        this.years = response.data.haveYears; // 응답에서 haveYears 배열 사용
+        this.availableYears = this.years.map(yearObj => parseInt(yearObj.year, 10)); // 사용 가능한 연도 배열로 저장
+        // console.log('Available years:', this.availableYears);
+        if (this.years.length > 0) {
+          this.selectedYear = this.availableYears[0];
+          this.fetchData();
+        }
+      } catch (error) {
+        console.error('Error fetching years:', error);
       }
     },
-    methods: {
-      async fetchYears() {
-        const params = new URLSearchParams();
-        params.append('member_Id', '1'); // member_Id로 설정
+    async fetchData() {
+      // console.log("Selected year: ", this.selectedYear); // 현재 선택한 년도 로그 출력
+      const params = new URLSearchParams();
+      params.append('member_Id', '1'); // member_Id로 설정
+      params.append('year', this.selectedYear); // selectedYear로 설정
   
-        try {
-          const response = await axios.get(`/api/record/year?${params.toString()}`);
-          console.log('Year response data:', response.data);
-          this.years = response.data.haveYears.map(record => record.year); // 응답에서 haveYears 배열 사용
-          if (this.years.length > 0) {
-            this.selectedYear = this.years[0];
-            this.fetchData();
+      try {
+        const response = await axios.get(`/api/record/chartMonth?${params.toString()}`);
+        // console.log('Expense response data:', response.data);
+        this.expenses = response.data.chartMonth.map((item, index, array) => {
+          let change = 0;
+          if (index > 0) {
+            change = item.amount - array[index - 1].amount;
           }
-        } catch (error) {
-          console.error('Error fetching years:', error);
-        }
-      },
-      async fetchData() {
-        console.log("Selected year: ", this.selectedYear); // 현재 선택한 년도 로그 출력
-        const params = new URLSearchParams();
-        params.append('member_Id', '1'); // member_Id로 설정
-        params.append('year', this.selectedYear); // selectedYear로 설정
-  
-        try {
-          const response = await axios.get(`/api/record/chartMonth?${params.toString()}`);
-          console.log('Expense response data:', response.data);
-          this.expenses = response.data.chartMonth.map((item, index, array) => {
-            let change = 0;
-            if (index > 0) {
-              change = item.amount - array[index - 1].amount;
-            }
-            return {
-              month: item.months,
-              amount: item.amount,
-              change: change
-            };
-          });
-          this.updateChart();
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      },
-      updateChart() {
-        const ctx = document.getElementById('expenseChart').getContext('2d');
-        if (this.chartInstance) {
-          this.chartInstance.destroy(); // 기존 차트가 있으면 파괴
-        }
-        this.chartInstance = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: this.expenses.map(expense => `${expense.month}월`),
-            datasets: [
-              {
-                label: '월별 소비 금액',
-                data: this.expenses.map(expense => expense.amount),
-                fill: true,
-                backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                borderColor: 'rgba(255, 159, 64, 1)',
-                borderWidth: 1,
-              },
-            ],
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true,
-              },
+          return {
+            month: item.months,
+            amount: item.amount,
+            change: change
+          };
+        });
+        this.updateChart();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    incrementYear() {
+      const nextYear = parseInt(this.selectedYear, 10) + 1;
+      // console.log('Trying to increment to year:', nextYear);
+      if (this.availableYears.includes(nextYear)) {
+        this.selectedYear = nextYear;
+        this.fetchData();
+      } else {
+        // console.log('Year not available:', nextYear);
+      }
+    },
+    decrementYear() {
+      const prevYear = parseInt(this.selectedYear, 10) - 1;
+      // console.log('Trying to decrement to year:', prevYear);
+      if (this.availableYears.includes(prevYear)) {
+        this.selectedYear = prevYear;
+        this.fetchData();
+      } else {
+        // console.log('Year not available:', prevYear);
+      }
+    },
+    updateChart() {
+      const ctx = document.getElementById('expenseChart').getContext('2d');
+      if (this.chartInstance) {
+        this.chartInstance.destroy(); // 기존 차트가 있으면 파괴
+      }
+      this.chartInstance = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: this.expenses.map(expense => `${expense.month}월`),
+          datasets: [
+            {
+              label: '월별 소비 금액',
+              data: this.expenses.map(expense => expense.amount),
+              fill: true,
+              backgroundColor: 'rgba(255, 159, 64, 0.2)',
+              borderColor: 'rgba(255, 159, 64, 1)',
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: false,
             },
           },
-        });
-      }
-    },
-    mounted() {
-      this.fetchYears();
+        },
+      });
     }
-  };
-  </script>
+  },
+  mounted() {
+    this.fetchYears();
+  }
+};
+</script>
   
 <style scoped>
-.container{
-      width:100%;
-      height:fit-content;
-  
-      display: flex;
-      flex-direction: column;
-      justify-content: start;
-  
-      padding: 0px;
-  
-      font-size:14px;
-  
-      /* 테스트 코드 */
-      /* border: 1px solid red; */
-  }
-  
-  .title{
-      font-size:20px;
-      font-weight: 800;
-      text-align: left;
-      width:fit-content;
-  
-      background: linear-gradient(transparent 30%, #ffcb7c 30%);
-      display: inline-block;
-  
-      margin-bottom:20px;
-      /* border: 1px solid blue; */
-  }
+.container {
+  width: 100%;
+  height: fit-content;
 
-  
-  .chart-container {
-    width: 100%;
-    margin: 0 auto 20px auto;
-  }
-  
-  
-  .list-container {
-    display: flex;
-    justify-content: space-around;
-    margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
 
-    font-size: 14px;
-  }
-  
-  .list-column {
-    width: 48%;
-  }
-  
-  .expense-item {
-    display: flex;
-    justify-content: space-between;
+  padding: 0px;
 
-    border:1px solid red;
-  }
-  
-  .expense-item span {
-    flex: 1;
-    text-align: center;
-  }
-  
-  .expense-item .month {
-    font-weight: bold;
-    color: #ff9f40;
-  }
-  
-  .expense-item .amount {
-    font-weight: bold;
-  }
-  
-  .up {
-    color: red;
-  }
-  
-  .down {
-    color: blue;
-  }
-  </style>
+  font-size: 14px;
+
+  /* 테스트 코드 */
+  /* border: 1px solid red; */
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 800;
+  text-align: left;
+  width: fit-content;
+
+  background: linear-gradient(transparent 30%, #ffcb7c 30%);
+  display: inline-block;
+
+  margin-bottom: 20px;
+  /* border: 1px solid blue; */
+}
+
+.chart-container {
+  width: 100%;
+  margin: 0 auto 20px auto;
+}
+
+.list-container {
+  display: flex;
+  justify-content: space-around;
+
+  font-size: 14px;
+}
+
+.list-column {
+  width: 48%;
+}
+
+.expense-item {
+  display: flex;
+  justify-content: space-between;
+  border-radius: 0px;
+  padding: 0;
+  background-color: white;
+  box-shadow: none;
+
+  /* border:1px solid red; */
+}
+
+.expense-item span {
+  flex: 1;
+  text-align: center;
+  /* border: 1px solid red; */
+}
+
+.expense-item .month {
+  font-weight: bold;
+  color: #ff9f40;
+}
+
+.expense-item .amount {
+  font-weight: bold;
+  min-width: 65%;
+}
+
+.change {
+  max-width: 10%;
+}
+
+.up {
+  color: red;
+}
+
+.down {
+  color: blue;
+}
+
+.year-selection {
+  display: flex;
+  justify-content: center;
+  margin-bottom:10px;
+}
+
+.year-selection > span {
+  font-size: 16px;
+  font-weight: 700;
+  text-align: center;
+
+  background-color: #fde5c1;
+  border-radius: 100px;
+
+  margin: 0px 10px;
+
+  width: 100px;
+}
+
+.year-selection button {
+  background-color: white;
+  border: none;
+}
+
+button .icon {
+  font-size: 14px;
+}
+</style>
