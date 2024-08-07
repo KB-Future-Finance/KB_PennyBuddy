64a00c5e1247a9e326d1d20bd5a1e9817a4248de
testjd1 <jaedong222@naver.com>
2024-08-07 23:49:37 +0900

    fix: ExpenseList, Input, Filter 화면 보완

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import TitleView from '../views/IntroView.vue'
 import ChartMonth from '@/components/Main/ChartMonth.vue';
 import ChartName from '@/components/Main/ChartName.vue';
 import DetailComponent from '@/components/ExpenseList/DetailComponent.vue'
+import InputFormComponent from '@/components/Input/InputFormComponent.vue';
+import ExpenseList from '@/components/ExpenseList/ExpenseList.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,9 +26,9 @@ const router = createRouter({
       component: () => import('../views/StyleView.vue')
     },
     {
-      path: '/expense-list',
-      name: 'expense-list',
-      component: () => import('../views/ListView.vue')
+      path: '/filter',
+      name: 'filter',
+      component: () => import('@/components/ExpenseList/FilterComponent.vue')
     },
     {
       path: '/chartmonth',
@@ -38,9 +40,27 @@ const router = createRouter({
       name: 'chartName',
       component: ChartName
     },
-    { path: '/detail/:recordIdx/:memberId', 
+    { 
+      path: '/detail/:recordIdx/:memberId', 
       component: DetailComponent, 
-      name: 'Detail' }
+      name: 'Detail' 
+    },
+    {
+      path: '/style',
+      name: 'style',
+      component: () => import('../views/StyleView.vue')
+    },
+    {
+      path: '/inputFormComponent',
+      name: 'inputFormComponent',
+      component: InputFormComponent
+    },
+    {
+      path: '/expense-list',
+      name: 'expense-list',
+      component: ExpenseList
+    }
+
     
   ]
 })
